c22dfbe9729ac740dd4092ad8da2ada7b8245d33
Alex <53349658+AlexPavlenko91@users.noreply.github.com>
2021-02-04 01:12:25 +0200

    SayHelloArgumentWrapperTest.php - done

--- tests/SayHelloArgumentWrapperTest.php
+++ tests/SayHelloArgumentWrapperTest.php
@@ -4,18 +4,11 @@ use PHPUnit\Framework\TestCase;
 
 class SayHelloArgumentWrapperTest extends TestCase
 {
-    /**
-     * @param $expected
-     * @param $input
-     */
-
-    public function testPositive($expected, $input)
-    {
-        $this->assertEquals($expected, sayHelloArgumentWrapper($input));
-    }
-
-    public function testException(): void
+    public function testException()
     {
         $this->expectException(InvalidArgumentException::class);
-    }#
+        sayHelloArgumentWrapper([1,2,3]);
+        sayHelloArgumentWrapper([1,2,['first', 'second']]);
+        sayHelloArgumentWrapper((object)[1,2,3]);
+    }
 }
